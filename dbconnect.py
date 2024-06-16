de4c601799a078f44c77515793a526941013f8a5
mlvalencia <laia.valencia1@gmail.com>
2024-06-17 04:59:37 +0800

    add all

--- dbconnect.py
+++ dbconnect.py
@@ -16,31 +16,31 @@ import pandas as pd
 #    # return the connection details
 #    return db
 
-#uncomment for prod:
+# #uncomment for prod:
 import os
 def getdblocation():
     DATABASE_URL = os.environ['DATABASE_URL']
     db = psycopg2.connect(DATABASE_URL, sslmode='require')
     return db
 
-def modifydatabase(sql, values):
-   db = getdblocation()
-######################
+# def modifydatabase(sql, values):
+#    db = getdblocation()
+# ######################
 
-   # We create a cursor object
-   # Cursor - a mechanism used to manipulate db objects on a per-row basis
-   # In this case, a cursor is used to add/edit each row
-   cursor = db.cursor()
-
-
-   # Execute the sql code with the cursor value
-   cursor.execute(sql, values)
+   # # We create a cursor object
+   # # Cursor - a mechanism used to manipulate db objects on a per-row basis
+   # # In this case, a cursor is used to add/edit each row
+   # cursor = db.cursor()
 
 
-   # Make the changes to the db persistent
-   db.commit()
-   # Close the connection (so nobody else can use it)
-   db.close()
+   # # Execute the sql code with the cursor value
+   # cursor.execute(sql, values)
+
+
+   # # Make the changes to the db persistent
+   # db.commit()
+   # # Close the connection (so nobody else can use it)
+   # db.close()
 
 
 
